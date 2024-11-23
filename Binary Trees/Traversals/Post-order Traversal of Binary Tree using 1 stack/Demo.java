3e8eec4b06ef337f67bab1b95483ff870b0fd3ce
Spyder <usubhadeep123@gmail.com>
2024-11-24 00:47:06 +0530

    done Post-order Traversal of Binary Tree using 1 stack

--- Binary Trees/Traversals/Post-order Traversal of Binary Tree using 1 stack/Demo.java
+++ Binary Trees/Traversals/Post-order Traversal of Binary Tree using 1 stack/Demo.java
@@ -16,18 +16,43 @@ class TreeNode {
 }
 
 class Solution {
+    // public List<Integer> postorderTraversal(TreeNode root) {
+    //     List<Integer> list = new ArrayList<>();
+    //     Stack<TreeNode> st = new Stack<>();
+
+    //     if(root == null) return list;
+
+    //     st.push(root);
+    //     while(!st.empty()) {
+    //         root = st.pop();
+    //         list.add(0, root.val);
+    //         if(root.left != null) st.push(root.left);
+    //         if(root.right != null) st.push(root.right);
+    //     }
+    //     return list;
+    // }
+    
     public List<Integer> postorderTraversal(TreeNode root) {
+        // using one stack
         List<Integer> list = new ArrayList<>();
         Stack<TreeNode> st = new Stack<>();
 
-        if(root == null) return list;
-
-        st.push(root);
-        while(!st.empty()) {
-            root = st.pop();
-            list.add(0, root.val);
-            if(root.left != null) st.push(root.left);
-            if(root.right != null) st.push(root.right);
+        while (root != null || !st.empty()) {
+            while (root != null) {
+                st.push(root);
+                root = root.left;
+            }
+            if(st.peek().right != null) {
+                root = st.peek().right;
+            }
+            else {
+                do {
+                    root = st.pop();
+                    list.add(root.val);
+                } while(!st.empty() && st.peek().right == root);
+                
+                root = null;
+            }
         }
         return list;
     }
