b88f47144993ef151699d4ec90cf36dff54a39b4
Spyder <usubhadeep123@gmail.com>
2024-11-25 23:49:44 +0530

    done Right View of Binary Tree

---
+++ Binary Trees/Medium Problems/Right View of Binary Tree/Demo.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    void dfs(TreeNode root, int row, List<Integer> res) {
+        if(root == null) return;
+
+        if(row == res.size())
+            res.add(root.val);
+        else
+            res.set(row, root.val);
+
+        dfs(root.left, row + 1, res);
+        dfs(root.right, row + 1, res);
+    }
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if(root == null) return res;
+
+        dfs(root, 0, res);
+
+        return res;
+    }
+}
