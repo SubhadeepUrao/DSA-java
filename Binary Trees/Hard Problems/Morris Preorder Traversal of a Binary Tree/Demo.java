3d5e2beed6b62cb1d9563c5feafc92e0dade541a
Spyder <usubhadeep123@gmail.com>
2024-11-29 21:08:36 +0530

    done Morris Preorder Traversal of a Binary Tree

---
+++ Binary Trees/Hard Problems/Morris Preorder Traversal of a Binary Tree/Demo.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// time complexity : O(N)
+// space complexity : O(1)
+// concept of threaded binary tree used
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+class Solution {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+
+        List<Integer> res = new ArrayList<>();
+
+        while(root != null) {
+            if(root.left == null) {
+                res.add(root.val);
+                root = root.right;
+            }
+            else {
+                TreeNode prev = root.left;
+
+                while(prev.right != null && prev.right != root)
+                    prev = prev.right;
+                
+                if(prev.right == null) {
+                    prev.right = root;
+                    res.add(root.val);
+                    root = root.left;
+                }
+                else {
+                    prev.right = null;
+                    root = root.right;
+                }
+            }
+        }
+        return res;
+    }
+}
