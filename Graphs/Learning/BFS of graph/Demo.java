8b22f74cd1fbde721df41e97cc8787ece3cb3c81
Spyder <usubhadeep123@gmail.com>
2024-12-05 01:05:24 +0530

    done BFS of graph

--- Graphs/Learning/BFS of graph/Demo.java
+++ Graphs/Learning/BFS of graph/Demo.java
@@ -3,23 +3,44 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 class Solution {
-
     public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
         ArrayList<Integer> bfs = new ArrayList<>();
         Queue<Integer> q = new LinkedList<>();
         boolean[] visited = new boolean[V];
-
+        
         q.add(0);
-        while (!q.isEmpty()) {
+        visited[0] = true;
+        
+        while(!q.isEmpty()) {
             int currNode = q.poll();
-            if (!visited[currNode]) {
-                bfs.add(currNode);
-                visited[currNode] = true;
-                for (int node : adj.get(currNode))
-                    if (!visited[node])
-                        q.add(node);
+            bfs.add(currNode);
+            
+            for(int node : adj.get(currNode)) {
+                if(!visited[node]) {
+                    visited[node] = true;
+                    q.add(node);
+                }
             }
         }
         return bfs;
     }
+
+    // public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+    //     ArrayList<Integer> bfs = new ArrayList<>();
+    //     Queue<Integer> q = new LinkedList<>();
+    //     boolean[] visited = new boolean[V];
+
+    //     q.add(0);
+    //     while (!q.isEmpty()) {
+    //         int currNode = q.poll();
+    //         if (!visited[currNode]) {
+    //             bfs.add(currNode);
+    //             visited[currNode] = true;
+    //             for (int node : adj.get(currNode))
+    //                 if (!visited[node])
+    //                     q.add(node);
+    //         }
+    //     }
+    //     return bfs;
+    // }
 }
