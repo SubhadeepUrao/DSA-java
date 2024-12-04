9b66a599098fd9f2ab9b5f03d4fd3854f05bc80a
Spyder <usubhadeep123@gmail.com>
2024-12-04 15:45:02 +0530

    done Number of Undirected Graphs with N vertices

---
+++ Graphs/Learning/Number of Undirected Graphs with N vertices/Demo.java
@@ -0,0 +1,18 @@
+class Solution {
+    // maximum edges a graph of n vertices can have n(n-1)/2 edges
+    // no. of graphs that can be formed with n vertices: [let e = n(n-1)/2]
+    //     n vertices with 0 edge : eC0 graphs (in nCr form)
+    //     n vertices with 1 edge : eC1 graphs
+    //     n vertices with 2 edge : eC2 graphs
+    //     .
+    //     .
+    //     n vertices with e edge : eCe graphs
+    // ------------------------------------
+    //               total graphs : 2^e graphs
+    // ------------------------------------
+
+
+    static long count(int n) {
+        return (long) Math.pow(2, n * (n - 1) / 2);
+    }
+}
