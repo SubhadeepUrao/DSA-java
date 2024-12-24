939a98e35f09e2ea7783f98f7cc522ebc901adda
Spyder <usubhadeep123@gmail.com>
2024-12-24 18:58:58 +0530

    done House Robber II

--- Dynamic Programming/1D DP/House Robber II/Memoization.java
+++ Dynamic Programming/1D DP/House Robber II/Memoization.java
@@ -1,13 +1,13 @@
 import java.util.Arrays;
 
 class Solution {
-    int rob(int[] nums, int i, int end, int[] dp) {
-        if(i < end) return 0;
+    int rob(int[] nums, int i, int start, int[] dp) {
+        if(i < start) return 0;
 
         if(dp[i] != -1) return dp[i];
 
-        int take = rob(nums, i-2, end, dp) + nums[i];
-        int notTake = rob(nums, i-1, end, dp);
+        int take = rob(nums, i-2, start, dp) + nums[i];
+        int notTake = rob(nums, i-1, start, dp);
 
         return dp[i] = Math.max(take, notTake);
     }
@@ -19,7 +19,7 @@ class Solution {
         if(N == 1) return nums[0];
 
         int[] dp = new int[N];
-        
+
         // end is inclusive
         Arrays.fill(dp, -1);
         int excludeLast = rob(nums, N-2, 0, dp); // exclude last house
