e7759d04b2e0d1a9208c8db82c4837e5e16f4c31
Spyder <usubhadeep123@gmail.com>
2025-01-03 15:51:50 +0530

    done Print Longest Increasing Subsequence

---
+++ Dynamic Programming/DP on LIS/Print Longest Increasing Subsequence/Optimal2.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Optimal2 {
+    // Index-wise lexicographically smallest
+    public static ArrayList<Integer> longestIncreasingSubsequence(int N, int nums[]) {
+        int[] dp = new int[N];
+        int[] previous = new int[N];
+        
+        Arrays.fill(dp, 1);
+        Arrays.fill(previous, -1);
+        int maxi = 0;
+        dp[0] = 1;
+
+        for(int i = 1; i < N; ++i) {
+            for(int prev = 0; prev < i; ++prev) {
+                if(nums[prev] < nums[i] && dp[i] < dp[prev] + 1) {
+                    dp[i] = dp[prev] + 1;
+                    previous[i] = prev;
+                }
+            }
+            if(dp[maxi] < dp[i])
+                maxi = i;
+        }
+
+        int i = maxi;
+        ArrayList<Integer> seq = new ArrayList<>();
+
+        seq.add(nums[maxi]);
+        while(previous[i] != -1) {
+            i = previous[i];
+            seq.add(0, nums[i]);
+        }
+        return seq;
+    }
+}
