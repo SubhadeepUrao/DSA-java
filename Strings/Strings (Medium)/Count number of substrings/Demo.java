377412aa793ea3a12ed1af965af25c0467c11bf7
Spyder <usubhadeep123@gmail.com>
2025-07-01 16:48:22 +0530

    added comments

--- Strings/Strings (Medium)/Count number of substrings/Demo.java
+++ Strings/Strings (Medium)/Count number of substrings/Demo.java
@@ -3,46 +3,52 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
+// count number of substrings with k distinct characters
+
 public class Demo {
 
-    public static int findSubstringCount(String s, int k) {
-        int n = s.length();
-        int[] freq = new int[26];
-        int cnt = 0;
-        int substringCnt = 0;
+  public static int findSubstringCount(String s, int k) {
+    int n = s.length();
+    int[] freq = new int[26];
+    int cnt = 0;
+    int substringCnt = 0;
 
-        int left = 0;
-        int right = 0;
-        for(left = 0; left < n; ++left ) {
-            while(right < n && cnt < k) {
-                freq[s.charAt(right)-97]++;
-                if(freq[s.charAt(right)-97] == 1) cnt++;
-                right++;
-            }
-            if(cnt == k) {
-                substringCnt += n - right + 1;
-            }
-            freq[s.charAt(left)-97]--;
-            if(freq[s.charAt(left)-97] == 0) cnt--;
-        }
-        return substringCnt;    
+    int left = 0;
+    int right = 0;
+    for (left = 0; left < n; ++left) {
+      while (right < n && cnt < k) {
+        freq[s.charAt(right) - 97]++;
+        if (freq[s.charAt(right) - 97] == 1) cnt++;
+        right++;
+      }
+      if (cnt == k) {
+        substringCnt += n - right + 1;
+      }
+      freq[s.charAt(left) - 97]--;
+      if (freq[s.charAt(left) - 97] == 0) cnt--;
     }
-    public static int substrCount(String s, int k) {
-        return findSubstringCount(s, k) - findSubstringCount(s, k+1);
-    }
-    public static void main(String args[]) throws IOException {
-        File inputfile = new File("input.txt");
-        Scanner input = new Scanner(inputfile);
-        FileWriter output = new FileWriter("output.txt");
+    return substringCnt;
+  }
 
-        String s = input.next();
-        int k = input.nextInt();
+  public static int substrCount(String s, int k) {
+    // #substrings with atleast k distinct characters - #substrings with atleast k+1
+    // distinct characters = #substrings with k distinct characters
+    return findSubstringCount(s, k) - findSubstringCount(s, k + 1);
+  }
 
-        int res = substrCount(s, k);
+  public static void main(String args[]) throws IOException {
+    File inputfile = new File("input.txt");
+    Scanner input = new Scanner(inputfile);
+    FileWriter output = new FileWriter("output.txt");
 
-        output.write(Integer.toString(res));
+    String s = input.next();
+    int k = input.nextInt();
 
-        input.close();
-        output.close();
-    }
+    int res = substrCount(s, k);
+
+    output.write(Integer.toString(res));
+
+    input.close();
+    output.close();
+  }
 }
