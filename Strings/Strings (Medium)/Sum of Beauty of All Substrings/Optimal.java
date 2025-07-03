a5cea06708361ad2b76a3b7a8329caa29bbfc39b
Spyder <usubhadeep123@gmail.com>
2025-07-03 14:58:35 +0530

    updated optimal

--- Strings/Strings (Medium)/Sum of Beauty of All Substrings/Optimal.java
+++ Strings/Strings (Medium)/Sum of Beauty of All Substrings/Optimal.java
@@ -1,55 +1,55 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Optimal {
 
-    public static int beautySum(String s) {
-        final int C = 26;
-        final int N = s.length();
-        final char[] chars = s.toCharArray();
+  public static int beautySum(String s) {
+    final int C = 26;
+    final int N = s.length();
+    final char[] chars = s.toCharArray();
 
-        int[] freq = new int[C];     // freq[ch-'a'] = frequency of ch in current substring
-        int[] histo = new int[N+1];  // histo[f] = number of chars with frequency f; 1<=f<=N.
+    int[] histo = new int[N + 1]; // histo[f] = number of chars with frequency f; 1<=f<=N.
+    
+    int sum = 0;
+    for (int i = 0; i < N; i++) {
 
-        int sum=0;
-        for(int i=0; i<N; i++)
-        {
-            int min = 0, max = 0;
-            Arrays.fill(freq, 0);
-            // histo[f] is zeroed when f is reached
+      int min = 0, max = 0;
+      int[] freq = new int[C]; // freq[ch-'a'] = frequency of ch in current substring
 
-            for(int j=i; j<N; j++)
-            {
-                char ch = chars[j];
-                int f = ++freq[ch-'a'];
+      // Arrays.fill(freq, 0);
+      // histo[f] is zeroed when new max is found
 
-                if(f>max)
-                    histo[max = f] = 0; // zero new histo slot
-                ++histo[f];
+      for (int j = i; j < N; j++) {
+        char ch = chars[j];
+        int f = ++freq[ch - 'a'];
 
-                if(f==1 || --histo[f-1]==0 && f-1==min )
-                    min = f;
+        if (f > max) histo[max = f] = 0; // zero new histo slot
 
-                sum += (max-min);
-            }
-        }
-        return sum;
+        ++histo[f];
+
+        if (f == 1 || --histo[f - 1] == 0 && f - 1 == min)
+          min = f;
+
+        sum += max - min;
+      }
     }
-    public static void main(String args[]) throws IOException {
-        File inputfile = new File("input.txt");
-        Scanner input = new Scanner(inputfile);
-        FileWriter output = new FileWriter("output.txt");
+    return sum;
+  }
 
-        String s = input.next();
+  public static void main(String args[]) throws IOException {
+    File inputfile = new File("input.txt");
+    Scanner input = new Scanner(inputfile);
+    FileWriter output = new FileWriter("output.txt");
 
-        int res = beautySum(s);
+    String s = input.next();
 
-        output.write(Integer.toString(res));
+    int res = beautySum(s);
 
-        input.close();
-        output.close();
-    }
+    output.write(Integer.toString(res));
+
+    input.close();
+    output.close();
+  }
 }
