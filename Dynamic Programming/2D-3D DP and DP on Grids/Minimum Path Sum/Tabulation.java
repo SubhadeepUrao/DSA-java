17980d96c8f7a0451b3fcbb89d2c5810357b0e86
Spyder <usubhadeep123@gmail.com>
2026-09-22 16:49:57 +0530

    updated Min Path Sum

--- Dynamic Programming/2D-3D DP and DP on Grids/Minimum Path Sum/Tabulation.java
+++ Dynamic Programming/2D-3D DP and DP on Grids/Minimum Path Sum/Tabulation.java
@@ -4,17 +4,18 @@ class Solution {
         int N = grid[0].length;
         int[][] dp = new int[M][N];
 
-        for(int i = 1; i < M; ++i)
-            dp[i][0] += dp[i-1][0];
-        for(int j = 1; j < N; ++j)
-            dp[0][j] += dp[0][j-1];
-        
-        for(int i = 1; i < M; ++i) {
-            for(int j = 1; j < N; ++j) {
-                dp[i][j] = Math.min(dp[i][j-1], dp[i-1][j]) + grid[i][j];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < M; ++i)
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        for (int j = 1; j < N; ++j)
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+
+        for (int i = 1; i < M; ++i) {
+            for (int j = 1; j < N; ++j) {
+                dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j];
             }
         }
 
-        return dp[M-1][N-1];
+        return dp[M - 1][N - 1];
     }
 }
